7785d9c8499888be42fe44f3385f3e393e2ac5ce
ivgtr <gear.1112t.o@gmail.com>
2020-08-07 08:01:08 +0900

    :tada: init: 環境構築+ログイン処理までの移行

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { getOauthToken, getAccessToken } from './oauth'
+
+const port = process.env.PORT || 5000
+
+const app = express()
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
+const wrap = (fn) => (...args) => fn(...args).catch(args[2])
+
+app.post(
+  '/getOauthToken',
+  wrap(async (req, res) => {
+    const data = await getOauthToken()
+    if (data.oauthToken && data.oauthTokenSecret) {
+      res.send(data)
+    } else {
+      const err = new Error(
+        'Error: Twitter APIに問題があるようです・・・時間を空けてからもう一度試してみてください。'
+      )
+      res.status(400).send({ error: err })
+    }
+  })
+)
+
+app.post(
+  '/getAccessToken',
+  wrap(async (req, res) => {
+    const {
+      oauth_token: oauthToken,
+      oauth_token_secret: oauthTokenSecret,
+      oauth_verifier: oauthVerifier
+    } = req.body
+
+    const data = await getAccessToken(
+      oauthToken,
+      oauthTokenSecret,
+      oauthVerifier
+    )
+    if (data.accessToken && data.accessTokenSecret) {
+      res.send(data)
+    } else {
+      const err = new Error(
+        'Error: 入力されたトークンに問題があるようです...もう一度最初から入力してください。'
+      )
+      res.status(400).send({ error: err })
+    }
+  })
+)
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Internal Server Error')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+export default app
---
+++ src/oauth.ts
@@ -0,0 +1,99 @@
+import axios from 'axios'
+import crypto from 'crypto'
+import OAuth, { RequestOptions, Options } from 'oauth-1.0a'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const oauthUrl = 'https://api.twitter.com/oauth'
+const callbackUrl = 'https://oauth.ivgtr.me/oauthTwitterRedirect'
+
+const options: Options = {
+  consumer: {
+    key: process.env.TWITTER_CONSUMER_KEY,
+    secret: process.env.TWITTER_CONSUMER_SECRET_KEY
+  },
+  signature_method: 'HMAC-SHA1',
+  hash_function(baseString, key) {
+    return crypto.createHmac('sha1', key).update(baseString).digest('base64')
+  }
+}
+
+const oauthInstance = (): OAuth => {
+  return new OAuth(options)
+}
+
+export const getOauthToken = async (): Promise<{
+  oauthToken: string
+  oauthTokenSecret: string
+}> => {
+  const requestData: RequestOptions = {
+    url: `${oauthUrl}/request_token`,
+    method: 'POST',
+    data: { oauth_verifier: callbackUrl }
+  }
+
+  try {
+    const responce = await axios.post(
+      `${oauthUrl}/request_token`,
+      {},
+      {
+        headers: oauthInstance().toHeader(
+          oauthInstance().authorize(requestData)
+        )
+      }
+    )
+
+    const oauthToken: string = responce.data.split('&')[0].split('=')[1]
+    const oauthTokenSecret: string = responce.data.split('&')[1].split('=')[1]
+    return {
+      oauthToken,
+      oauthTokenSecret
+    }
+  } catch (err) {
+    return {
+      oauthToken: '',
+      oauthTokenSecret: ''
+    }
+  }
+}
+
+export const getAccessToken = async (
+  oauthToken: string,
+  oauthTokenSecret: string,
+  oauthVerifier: string
+): Promise<{
+  accessToken: string
+  accessTokenSecret: string
+}> => {
+  const requestData: RequestOptions = {
+    url: `${oauthUrl}/access_token`,
+    method: 'POST',
+    data: {
+      oauth_token: oauthToken,
+      oauth_token_secret: oauthTokenSecret,
+      oauth_verifier: oauthVerifier
+    }
+  }
+
+  try {
+    const responce = await axios.post(
+      `${oauthUrl}/access_token`,
+      {},
+      {
+        headers: oauthInstance().toHeader(
+          oauthInstance().authorize(requestData)
+        )
+      }
+    )
+    const accessToken: string = responce.data.split('&')[0].split('=')[1]
+    const accessTokenSecret: string = responce.data.split('&')[1].split('=')[1]
+
+    return { accessToken, accessTokenSecret }
+  } catch (err) {
+    return {
+      accessToken: '',
+      accessTokenSecret: ''
+    }
+  }
+}
